c2a5a349495535d686c37fa0537dcc49be181992
dli <davidli012345@gmail.com>
2019-06-15 15:24:01 -0700

    adding CI/CD

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # good old bash to .env files
-echo $TEST_MONGO_DB >> .env
-echo $NODE_ENV >> .env
+echo "NODE_ENV=$NODE_ENV" >> .env
+echo "MONGO_URI=$TEST_MONGO_DB" >> .env
+
+if [ "$1" != "" ]; then
+    echo "Positional parameter 1 contains something"
+    echo "DISCORD_TOKEN=$DISCORD_TOKEN" >> .env
+fi
