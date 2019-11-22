faa5bf5e33a42985ddcc5938e7284ba306ef4adc
dli <davidli012345@gmail.com>
2019-11-21 19:58:39 -0800

    tracking more stocks

--- discord/events/ready.ts
+++ discord/events/ready.ts
@@ -9,7 +9,7 @@ module.exports = async (client: any) => {
     // Get weather info and stock data
     const channel = client.channels.get("571860897887289410")
     const baseUrl = 'https://my-go-project.davidli012345.now.sh/api'
-    const quotes = 'quotes=BB.TO,NEXCF,ABT.TO,AT.TO,ACB.TO,WMT.TO,CGC,NXTTF'
+    const quotes = 'quotes=BB.TO,NEXCF,ABT.TO,AT.TO,ACB.TO,WMT.TO,CGC,NXTTF,OTCF,DDOG'
     fetch(`${baseUrl}/dsJSON.go?${quotes}`)
         .then((resp: { json: () => void; }) => {
             return resp.json()
